d6dd766b112801dac889348138769f9bd14ab66f
fallenlynx <bralynfagel@yahoo.com>
2016-04-07 17:37:25 -0400

    created port and screen init
    moved acceleromotor stuff to the port and screen inits

--- functions.c
+++ functions.c
@@ -62,19 +62,66 @@ void drawAccelData()
 }
 
 //----------------------------------------------------------------------------
-//	Initializing ports
-//----------------------------------------------------------------------------
-void port_init()
-{
-
-}
-
-//----------------------------------------------------------------------------
 //	Initializing the screen
 //----------------------------------------------------------------------------
 void screen_init()
 {
+	//	Initializes Clock System
+	MAP_CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_48);
+	MAP_CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+	MAP_CS_initClockSignal(CS_HSMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+	MAP_CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+	MAP_CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
 
+	//	Initializes display
+	Crystalfontz128x128_Init();
+
+	//	Set default screen orientation
+	Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP);
+
+	//	Initializes graphics context
+	Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128);
+	Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_RED);
+	Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_WHITE);
+	GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
+	drawTitle();
+
+}
+
+//----------------------------------------------------------------------------
+//	Initializing ports
+//----------------------------------------------------------------------------
+void port_init()
+{
+	//	Configures Pin 4.0, 4.2, and 6.1 as ADC inputs
+	MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P4, GPIO_PIN0 | GPIO_PIN2, GPIO_TERTIARY_MODULE_FUNCTION);
+	MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN1, GPIO_TERTIARY_MODULE_FUNCTION);
+
+	//	Initializing ADC (ADCOSC/64/8)
+	MAP_ADC14_enableModule();
+	MAP_ADC14_initModule(ADC_CLOCKSOURCE_ADCOSC, ADC_PREDIVIDER_64, ADC_DIVIDER_8, 0);
+
+	//	Configuring ADC Memory (ADC_MEM0 - ADC_MEM2 (A11, A13, A14) with no repeat) with internal 2.5v reference
+	MAP_ADC14_configureMultiSequenceMode(ADC_MEM0, ADC_MEM2, true);
+	MAP_ADC14_configureConversionMemory(ADC_MEM0, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A14, ADC_NONDIFFERENTIAL_INPUTS);
+
+	MAP_ADC14_configureConversionMemory(ADC_MEM1, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A13, ADC_NONDIFFERENTIAL_INPUTS);
+
+	MAP_ADC14_configureConversionMemory(ADC_MEM2, ADC_VREFPOS_AVCC_VREFNEG_VSS, ADC_INPUT_A11, ADC_NONDIFFERENTIAL_INPUTS);
+
+	//	Enabling the interrupt when a conversion on channel 2 (end of sequence) is complete and enabling conversions
+	MAP_ADC14_enableInterrupt(ADC_INT2);
+
+	//	Enabling Interrupts
+	MAP_Interrupt_enableInterrupt(INT_ADC14);
+	MAP_Interrupt_enableMaster();
+
+	//	Setting up the sample timer to automatically step through the sequence convert
+	MAP_ADC14_enableSampleTimer(ADC_AUTOMATIC_ITERATION);
+
+	//	Triggering the start of the sample
+	MAP_ADC14_enableConversion();
+	MAP_ADC14_toggleConversionTrigger();
 }
 
 //----------------------------------------------------------------------------
